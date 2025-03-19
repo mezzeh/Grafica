2b9fd8ee987877a61d59878cd88c282a675b4ff5
mezzeh <francimezze@gmail.com>
2025-03-19 22:22:23 +0100

    update

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-// Reindirizza direttamente alla pagina principale
-header("Location: /pages/index.php");
+// Includi il file di configurazione dei percorsi
+require_once __DIR__ . '/config/paths.php';
+
+// Reindirizza alla pagina principale
+header("Location: " . getUrlPath('pages/index.php'));
 exit;
 ?>
